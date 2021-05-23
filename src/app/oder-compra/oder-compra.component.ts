98382274fdd14f32ade9b07e3797ede50de069ba
GilbertoTADS <gilbertotads@gmail.com>
2021-05-23 18:37:16 -0300

    adiçãode feedback para usuário em formulario de compras

--- src/app/oder-compra/oder-compra.component.ts
+++ src/app/oder-compra/oder-compra.component.ts
@@ -10,6 +10,7 @@ import { Pedido } from '../shared/pedido.model'
   providers: [ OrdemCompraService ]
 })
 export class OderCompraComponent implements OnInit {
+  idPedidoCompra?:number;
   formulario:FormGroup = new FormGroup({
     'endereco': new FormControl(null, [ Validators.required, Validators.minLength(3), Validators.maxLength(120)]),
     'numero': new FormControl(null, [ Validators.required, Validators.minLength(1), Validators.maxLength(20)]),
@@ -23,6 +24,23 @@ export class OderCompraComponent implements OnInit {
   }
 
   public confirmarCompra(): void {
-    console.log(this.formulario)
+    if(this.formulario.invalid){
+      this.formulario.get('endereco').markAsTouched();
+      this.formulario.get('numero').markAsTouched();
+      this.formulario.get('complemento').markAsTouched();
+      this.formulario.get('formaPagamento').markAsTouched();
+    }else{
+      let pedido:Pedido = new Pedido(
+        this.formulario.value.endereco, 
+        this.formulario.value.numero,
+        this.formulario.value.complemento,
+        this.formulario.value.formaPagamento
+        );
+        this.ordemCompraService.efetivarCompra(pedido)
+          .subscribe( (idPedido:number) => {
+            this.idPedidoCompra = idPedido;
+            console.log(this.idPedidoCompra);
+          })
+    }
   }
 }
